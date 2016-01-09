280b706128d03a277ed083cd03000adeafb09a3b
Paul Boeck <paul.boeck@cassantec.com>
2016-01-09 19:29:54 +0100

    Merge branch 'master' of github.com:just-kile/blockwelt
    # Conflicts:
    #	src/frontend/app/controllers/mainController.js
    #	src/frontend/index.html

--- src/frontend/app/controllers/mainController.js
+++ src/frontend/app/controllers/mainController.js
@@ -1,4 +1,4 @@
-angular.module('blockweltapp').controller("MainController", function ($http, importService) {
+angular.module('blockweltapp').controller("MainController", function ($http, importService, projectionService) {
 
     this.upload = function (path) {
         $http({
@@ -36,25 +36,8 @@ angular.module('blockweltapp').controller("MainController", function ($http, imp
     map.addLayer(new ol.layer.Vector({
         source: vectorSource
     }));
-    this.paint_blocks = function () {
-        var style = new ol.style.Style({
-            fill: new ol.style.Fill({
-                color: 'rgba(255, 0, 0, 0.6)'
-            }),
-            stroke: new ol.style.Stroke({
-                color: '#319FD3',
-                width: 1
-            })
-        });
-        var polygon = ol.geom.Polygon.fromExtent([1400000, 6800000, 1600000, 6900000]);
-        var feature = new ol.Feature({
-            geometry: polygon
-        });
-        feature.setStyle(style);
-        vectorSource.addFeature(feature);
-    };
 
-    vm.with_data = function() {
+    this.with_data = function () {
         var grid = {
             longitude: 1400000,
             latitude: 6800000,
@@ -79,4 +62,4 @@ angular.module('blockweltapp').controller("MainController", function ($http, imp
         vectorSource.addFeatures(features);
 
     }
-);
+})
