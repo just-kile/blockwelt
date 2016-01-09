5b7bd9c83f6881527eced897782537623cdd5387
thomaskipar <thomas.kipar@gmail.com>
2016-01-09 18:36:21 +0100

    Using example data initially

--- src/frontend/app/controllers/mainController.js
+++ src/frontend/app/controllers/mainController.js
@@ -1,18 +1,38 @@
-angular.module('blockweltapp').controller("MainController", function($http){
-    var vm = this;
-    vm.welcome_message = "This is Blockwelt.";
-    var map = new ol.Map({
-        layers: [
-            new ol.layer.Tile({
-                source: new ol.source.OSM()
+angular.module('blockweltapp').controller("MainController", function($http, importService){
+
+    function createMap() {
+        var map = new ol.Map({
+            layers: [
+                new ol.layer.Tile({
+                    source: new ol.source.OSM()
+                })
+            ],
+            target: 'map',
+            view: new ol.View({
+                center: [0, 0],
+                zoom: 2
             })
-        ],
-        target: 'map',
-        view: new ol.View({
-            center: [0, 0],
-            zoom: 2
-        })
-    });
-    var graticule = new ol.Graticule();
-    graticule.setMap(map);
+        });
+        var graticule = new ol.Graticule();
+        graticule.setMap(map);
+    }
+
+    this.upload = function(path) {
+        $http({
+            method: 'GET',
+            url: path,
+        }).success(function(data){
+            var rawData = importService.importData(data);
+            console.log(rawData);
+            // TODO: Use the data, my little padawan
+        }).error(function() {
+            alert("error");
+        });
+    };
+
+    this.initialize = function() {
+        this.upload('../../../example/locations.json');
+    };
+
+    createMap();
 });
