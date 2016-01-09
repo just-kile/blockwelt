2ed05934cb7b53682d6de67bffeeeb257f69a074
Paul Boeck <paul.boeck@cassantec.com>
2016-01-09 13:16:47 +0100

    Move openlayer scripts to controller

--- src/frontend/app/controllers/mainController.js
+++ src/frontend/app/controllers/mainController.js
@@ -1,4 +1,18 @@
 angular.module('blockweltapp').controller("MainController", function($http){
     var vm = this;
     vm.welcome_message = "This is Blockwelt.";
+    var map = new ol.Map({
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM()
+            })
+        ],
+        target: 'map',
+        view: new ol.View({
+            center: [0, 0],
+            zoom: 2
+        })
+    });
+    var graticule = new ol.Graticule();
+    graticule.setMap(map);
 });
