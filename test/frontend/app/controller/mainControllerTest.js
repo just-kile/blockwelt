c0e80f9da480b31406b4a5a8272cbdc58f0c9fd0
Thomas Kipar <thomas.kipar@gmail.com>
2016-01-16 10:43:24 +0100

    Added first test for the MainController

---
+++ test/frontend/app/controller/mainControllerTest.js
@@ -0,0 +1,30 @@
+describe('Main Controller', function () {
+
+    var scope;
+    var mainController;
+
+    beforeEach(angular.mock.module("blockweltapp"));
+
+    beforeEach(function() {
+        inject(function($rootScope, $controller, $http, importService){
+            scope=$rootScope.$new();
+
+
+            mainController =$controller('MainController', {
+                $scope: scope,
+                $http: $http,
+                importService: importService
+            });
+        })
+    });
+
+    describe('When I show example data', function () {
+
+        it('sets progress to true', function () {
+            mainController.showExampleData();
+            expect(scope.model.progress).toEqual(true)
+        })
+
+    });
+
+});
