1867048af149215cd2083ae2e4c9ea1034232449
thomaskipar <thomas.kipar@gmail.com>
2016-02-14 17:42:41 +0100

    Added get request for shared data.

--- src/backend/routes.js
+++ src/backend/routes.js
@@ -1,5 +1,6 @@
 function setup(app, handlers) {
     app.post('/rest/share/upload', handlers.share.upload);
+    app.get('/rest/share/:id', handlers.share.get);
 }
 
 exports.setup = setup;
--- src/backend/shareHandler.js
+++ src/backend/shareHandler.js
@@ -2,8 +2,6 @@ var db;
 
 function upload(request, response) {
     if (request.body && request.body.locations) {
-        console.log(request.body.locations);
-        
         db.get('locations').insert({locations: request.body.locations}, function (error, data) {
             if (error) {
                 response.status(500).send(error);
@@ -16,8 +14,25 @@ function upload(request, response) {
     }
 }
 
+function get(request, response) {
+    var id = request.params.id;
+    console.log('asfasdf ', id)
+    if (id) {
+        db.get('locations').findOne({_id: id}, function (error, data) {
+            if (error) {
+                response.status(500).send(error);
+            } else {
+                response.status(200).json(data);
+            }
+        });
+    } else {
+        response.status(400).send();
+    }
+}
+
 module.exports = function (_db) {
     db = _db;
 
-    this.upload = upload
+    this.upload = upload,
+    this.get = get
 };
