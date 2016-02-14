12527df797514fa892319732726947cf5a0716fc
Paul Boeck <paul.boeck@cassantec.com>
2016-02-14 14:40:34 +0100

    Share button sends request to backend

--- src/frontend/app/controllers/shareDataController.js
+++ src/frontend/app/controllers/shareDataController.js
@@ -8,14 +8,14 @@ angular.module('blockweltapp').controller("ShareDataController", function ($http
     var req = {
         method: 'POST',
         url: 'rest/upload',
-        data: {test: 'test'}
-    }
+        data: {locations: $scope.model.locations}
+    };
 
     $scope.share = function() {
-        $http(req).then(function () {
-            alert('success')
+        $http(req).then(function (response) {
+            $scope.model.shareURL = response.data.id;
         }, function () {
-            alert('failure')
+            $scope.model.shareURL = "ERROR";
         });
     };
 
---
+++ test/frontend/app/controller/shareDataControllerTest.js
@@ -0,0 +1,64 @@
+describe('ShareData Controller', function () {
+
+    var scope;
+    var shareDataController;
+    var httpBackend;
+
+    const testLatitude = 525557393;
+    const testLongitude = 133418855;
+    const testId = '1000';
+
+    beforeEach(angular.mock.module("blockweltapp"));
+
+    beforeEach(function () {
+        inject(function ($rootScope, $controller, $http, importService, $httpBackend) {
+            scope = $rootScope.$new();
+
+            scope.model = {
+                locations: [{"latitude": testLatitude, "longitude": testLongitude}],
+                shareURL: ''
+            };
+
+            httpBackend = $httpBackend;
+
+            shareDataController = $controller('ShareDataController', {
+                $scope: scope,
+                $http: $http,
+                importService: importService
+            });
+        })
+    });
+
+    describe('When I share data', function () {
+        it('sends a post request to the backend', function () {
+            httpBackend.when('POST', 'rest/upload').respond(200, '');
+            httpBackend.expectPOST('rest/upload');
+            scope.share();
+            httpBackend.flush();
+        });
+    });
+
+    describe('When I share data with one location', function () {
+        it('sends the location the backend', function () {
+            var expectedData = {locations: [{"latitude": testLatitude, "longitude": testLongitude}]};
+            httpBackend.when('POST', 'rest/upload').respond(200, {id: testId});
+            httpBackend.expectPOST('rest/upload', expectedData);
+            scope.share();
+            httpBackend.flush();
+            expect(scope.model.shareURL).toEqual(testId);
+        });
+    });
+
+    describe('When I try to share data and it fails', function () {
+        it('shows an error message in the url field', function () {
+            httpBackend.when('POST', 'rest/upload').respond(500, '');
+            httpBackend.expectPOST('rest/upload');
+            scope.share();
+            httpBackend.flush();
+            expect(scope.model.shareURL).toEqual('ERROR');
+        });
+    });
+
+
+
+});
