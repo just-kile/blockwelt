e9877d645de7cbb3738789eb1277bd8537ee7b20
Paul Boeck <paul.boeck@cassantec.com>
2016-02-14 17:22:38 +0100

    Fix request

--- src/frontend/app/controllers/shareDataController.js
+++ src/frontend/app/controllers/shareDataController.js
@@ -4,13 +4,14 @@
 
 angular.module('blockweltapp').controller("ShareDataController", function ($http, $scope) {
 
-    var req = {
-        method: 'POST',
-        url: 'rest/share/upload',
-        data: {locations: $scope.model.locations}
-    };
 
     $scope.share = function() {
+        var req = {
+            method: 'POST',
+            url: 'rest/share/upload',
+            data: {locations: $scope.model.locations}
+        };
+
         $http(req).then(function (response) {
             $scope.model.shareURL = response.data.id;
         }, function () {
